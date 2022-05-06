c3a0172dbf36137cf6f07e5b0d5b564a80f224ae
joo9411 <102640100+joo9411@users.noreply.github.com>
2022-05-06 09:49:00 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         let skill = $(this).scrollTop();
         console.log(skill);
-        if(skill>=960){
+        if(skill>=950){
             $('.skill .s_left > li:first-child > .bar').stop().animate({'width':'90%'},800);
             $('.skill .s_left > li:nth-child(2) > .bar').stop().animate({'width':'85%'},800);
             $('.skill .s_left > li:last-child  > .bar').stop().animate({'width':'50%'},800);
@@ -182,4 +182,43 @@ $(document).ready(function(){
     $('.intro_modal').hide();
     return false;
     });
+
+
+    //스크롤 이벤트 서식
+    
+    window.onload = function () {
+        $(".box").each(function () {
+            // 개별적으로 Wheel 이벤트 적용
+            $(this).on("mousewheel DOMMouseScroll", function (e) {
+                e.preventDefault();
+                var delta = 0;
+                if (!event) event = window.event;
+                if (event.wheelDelta) {
+                    delta = event.wheelDelta / 120;
+                    if (window.opera) delta = -delta;
+                } else if (event.detail) delta = -event.detail / 3;
+                var moveTop = null;
+                // 마우스휠을 위에서 아래로
+                if (delta < 0) {
+                    if ($(this).next() != undefined) {
+                        moveTop = $(this).next().offset().top;
+                    }
+                // 마우스휠을 아래에서 위로
+                } else {
+                    if ($(this).prev() != undefined) {
+                        moveTop = $(this).prev().offset().top;
+                    }
+                }
+                // 화면 이동 0.8초(800)
+                $("html,body").stop().animate({
+                    scrollTop: moveTop + 'px'
+                }, {
+                    duration: 800, complete: function () {
+                    }
+                });
+            });
+        });
+    }
+
+
 });
